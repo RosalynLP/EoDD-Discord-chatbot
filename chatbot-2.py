b62b12c2716bd77d38793a78f292a40fc130e852
Ewan Klein <ewan@raw-text.io>
2021-05-28 11:27:08 +0100

    first import

---
+++ chatbot-2.py
@@ -0,0 +1,64 @@
+import json
+import os
+import random
+
+import discord
+from dotenv import load_dotenv  # required for running locally
+load_dotenv()
+
+with open("exchanges.json", "r") as f:
+    exchanges = json.load(f)
+
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client()
+
+talking = False
+
+
+def new_topic():
+    """
+    Start talking about a random topic.
+    """
+    topics = ["wasps", "ice cream", "school", "Fortnite",
+              "burgers", "hotels", "getting a suntan"]
+    choice = random.choice(topics)  # choose a topic at random
+    phrases = [f"So what do you think about {choice}?",
+               f"Anyway, I really like {choice}!", f"That's really interesing but what about {choice}???"]
+    phrase = choice = random.choice(phrases)
+    return phrase
+
+
+def respond(user_input):
+    """
+    Generate a response to the user input.
+    """
+    user_input.lower()
+    words = user_input.split()
+    response = new_topic()
+    for word in words:
+        if word in exchanges:
+            responses = exchanges[word]
+            response = random.choice(responses)
+            break
+    return response
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    user_input = message.content
+    if not talking and user_input in ["$hlo", "$hello", "hi"]:
+        talking = True
+    response = respond(user_input)
+    await message.reply(response)
+
+
+client.run(TOKEN)
